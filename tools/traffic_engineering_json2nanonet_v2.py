f701168559caaed9bbb0d7b7e47d89385835b539
Nikolaus Süß <nikolaus.suess@univie.ac.at>
2021-06-25 14:32:30 +0200

    Script for parsing the new JSON files for Traffic Engineering

---
+++ tools/traffic_engineering_json2nanonet_v2.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+import json
+import math
+from sys import stderr, argv
+
+# Factors for align the comma values of the JSON file
+# Please note, that CAPACITY and DEMAND are both in KB.
+WEIGHT_FACTOR   =   1000
+CAPACITY_FACTOR = 1000000000000
+DEMAND_FACTOR   = 10000
+# Please note: CAPACITY_FACTOR and DEMAND_FACTOR should ALWAYS be the same value!!!
+if CAPACITY_FACTOR != DEMAND_FACTOR:
+    stderr.write("WARNING: CAPACITY_FACTOR does not match DEMAND_FACTOR!\n")
+
+# Number of streams that are started. These streams are hashed individually.
+NSTREAMS =  32 # parallel streams
+TIME     = 300 # seconds
+
+# Please note:
+# The evaluation code was moved to a bash script.
+
+filename = "/dev/stdin"
+topo_name = "Thomas"
+if len(argv) >= 2:
+    filename = argv[1]
+if len(argv) >= 3:
+    topo_name = argv[2]
+
+nodes = []
+destination_nodes = []
+
+# Parse JSON file
+with open(filename) as json_file:
+    data = json.load(json_file)
+
+output = f"""
+#!/usr/bin/env python3
+from node import *
+
+class {topo_name}(Topo):
+    def build(self):
+"""
+
+# Get all node names and create the nodes
+for node in data["links"]:
+    nodes.append(node["i"])
+    nodes.append(node["j"])
+nodes = list(set(nodes))
+
+# get destinations
+for node in data["demands"]:
+    destination_nodes.append(node["dst"])
+destination_nodes = list(set(destination_nodes))
+
+for n in nodes:
+    output += \
+f"""\
+        self.add_node("{n}")
+"""
+
+for edge in data["links"]:
+    output += \
+f"""\
+        self.add_link_name("{edge["i"]}", "{edge["j"]}", cost={math.ceil(edge["weight"]*WEIGHT_FACTOR)}, delay=0.2, bw={edge["capacity"]*CAPACITY_FACTOR}, directed=True)
+"""
+
+output += \
+f"""\
+    
+    def dijkstra_computed(self):
+"""
+# Create routes out of the demands
+for routes in data["demands"]:
+    route_cmd = f"""\"ip -6 route add {{{routes["dst"]}}} metric 1 src {{{routes["src"]}}} """
+    for seg in routes["waypoint_chance_map"]:
+        route_cmd += f""" nexthop via "+self.get_dijkstra_route_by_name("{str(routes["src"])}","{str(seg)}")[0].nh +\""""
+        if str(seg) != str(routes['dst']):
+            route_cmd += f""" encap seg6 mode encap segs {{{str(seg)}}} """
+        route_cmd += f""" weight {int(routes["waypoint_chance_map"][str(seg)]*100)} """
+    route_cmd += "\""
+    output += \
+f"""\
+        # Demand from {routes["src"]} to {routes["dst"]}
+        self.add_command("{routes["src"]}", {route_cmd})
+"""
+
+# Start nuttcp at destination nodes
+for n in destination_nodes:
+    output += \
+f"""\
+        self.add_command("{n}", "nuttcp -6 -S")
+"""
+for demand in data["demands"]:
+    output += \
+f"""\
+        self.add_command("{demand["src"]}", 'echo bash -c \\\\\\\"START=\\\\\\\\\\$SECONDS\; while \! ip netns exec {demand["src"]} nuttcp -T{TIME} -i1 -R{int(demand["demand_size"]*DEMAND_FACTOR)} -N{NSTREAMS} {{{demand["dst"]}}} \>\>flow_{demand["src"]}-{demand["dst"]}.txt 2\>\&1 \; do sleep 1\; echo RTY\: \\\\\\\\\\$SECONDS \>\>flow_{demand["src"]}-{demand["dst"]}.txt\; done\\\\\\\" | at now+2min')
+"""
+
+output += """
+        self.enable_throughput()
+"""
+
+output += f"""
+topos = {{'{topo_name}': (lambda: {topo_name}())}}
+"""
+
+print(output)
